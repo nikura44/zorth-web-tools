52025e5f93a6dfa12dc8d6597c766de38faa0473
Nikuras4444 <68015316@qq.com>
2022-12-10 16:18:52 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    @select="handleSelect"
+    router="true"
+  >
+    <el-menu-item index="0">LOGO</el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="/">Processing Center</el-menu-item>
+    <el-menu-item index="/about"> Processing Center </el-menu-item>
+  </el-menu>
+  <router-view></router-view>
 </template>
 
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const activeIndex = ref('/');
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.flex-grow {
+  flex-grow: 1;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import vue3videoPlay from 'vue3-video-play'; // 引入组件
+import 'vue3-video-play/dist/style.css'; // 引入css
 
-createApp(App).use(router).mount('#app')
+import router from './router';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.use(ElementPlus);
+app.use(vue3videoPlay);
+app.use(router);
+app.mount('#app');
