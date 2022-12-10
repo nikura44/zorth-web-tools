92cb5d190ca3297d93ebe9f1fe4aea47faadea65
Nikuras4444 <68015316@qq.com>
2022-12-10 16:55:33 +0800

    video big break

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,36 +1,27 @@
 <template>
-  <div class="about">
-    <VideoPlayer
-      v-bind="options"
-      @play="onPlay"
-      @pause="onPause"
-      @timeupdate="onTimeupdate"
-      @canplay="onCanplay"
-    />
+  <div>
+    <vue3VideoPlay v-bind="options" poster='https://go.dreamwq.com/videos/ironMan.jpg'/>
   </div>
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue';
-import { VideoPlayer } from 'vue-video-player';
-
 const options = reactive({
-  width: '800px', // 播放器高度
-  height: '450px', // 播放器高度
-  color: '#409eff', // 主题色
-  muted: false, // 静音
+  width: '800px', //播放器高度
+  height: '450px', //播放器高度
+  color: "#409eff", //主题色
+  title: '', //视频名称
+  src: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4", //视频源
+  muted: false, //静音
   webFullScreen: false,
-  speedRate: ['1.0', '1.25'], // 播放倍速
-  autoPlay: false, // 自动播放
-  loop: false, // 循环播放
-  mirror: false, // 镜像画面
-  ligthOff: false, // 关灯模式
-  volume: 0.3, // 默认音量大小
-  control: true, // 是否显示控制器
-  title: '123', // 视频名称
-  src: 'http://vjs.zencdn.net/v/oceans.mp4', // 视频源
-  poster: '', // 封面
-});
+  speedRate: ["0.75", "1.0", "1.25", "1.5", "2.0"], //播放倍速
+  autoPlay: false, //自动播放
+  loop: false, //循环播放
+  mirror: false, //镜像画面
+  ligthOff: false,  //关灯模式
+  volume: 0.3, //默认音量大小
+  control: true, //是否显示控制器
+})
 </script>
 
 <style scoped>
